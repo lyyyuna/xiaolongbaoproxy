88a66fc994c2b518b00cd9b0b5189cc769cbdfa7
lyyyuna <lyyyuna@outlook.com>
2019-07-16 14:43:25 +0800

    add profile code

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,34 @@ import (
 	"flag"
 	"fmt"
 	"goproxy/pkg/proxy"
+	"os"
+	"os/signal"
+	"runtime/pprof"
+	"syscall"
 )
 
 func main() {
 	port := flag.String("port", "8080", "Listening port")
 	logpath := flag.String("log", "mitm.log", "Specify where to store the log")
+	profile := flag.String("profile", "", "write cpu profile to file")
 
 	flag.Parse()
+	if *profile != "" {
+		f, err := os.Create(*profile)
+		if err != nil {
+			fmt.Println("Profile error")
+			return
+		}
+		pprof.StartCPUProfile(f)
+		c := make(chan os.Signal)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+		go func() {
+			<-c
+			fmt.Println("exiting")
+			pprof.StopCPUProfile()
+			os.Exit(1)
+		}()
+	}
 
 	fmt.Println("The proxy is listening on port: ", *port)
 	fmt.Println("Log will be written to: ", *logpath)
