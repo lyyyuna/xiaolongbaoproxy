1b317ef5327a7a8ee3146bc1b7960e5475713d16
lyyyuna <lyyyuna@outlook.com>
2020-02-29 21:11:03 +0800

    add server/port command option

--- cmd/basic.go
+++ cmd/basic.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"net"
 	"net/http"
 	"xiaolongbaoproxy/internal"
 )
@@ -12,7 +15,31 @@ var basicCmd = &cobra.Command{
 	Run:   runProxy,
 }
 
+var (
+	host string
+	port int
+)
+
+func init() {
+	basicCmd.Flags().StringVarP(&host, "host server address", "s", "0.0.0.0", "Specify the host server address.")
+	basicCmd.Flags().IntVarP(&port, "port number", "p", 8080, "Specify the port number.")
+}
+
+func checkValidAddrPort(host string, port int) {
+	if net.ParseIP(host) == nil {
+		panic("IP address is invalid.")
+	}
+
+	if port < 1 || port > 65535 {
+		panic("Invalid port address.")
+	}
+}
+
 func runProxy(cmd *cobra.Command, args []string) {
+	checkValidAddrPort(host, port)
+
+	addr := fmt.Sprintf("%v:%v", host, port)
+	log.Infof("Proxy server is hosting on %v", addr)
 	p := internal.NewProxyHttpServer()
-	http.ListenAndServe(":8080", p)
+	http.ListenAndServe(addr, p)
 }
